c69109632887a3410b07a98254be31216cf5f0a1
Nantawat Sukrisunt <nantawat.suk@ku.th>
2025-04-25 22:16:58 +0700

    add todo model and list view

--- todo_app/views/todo/todo_list_view.py
+++ todo_app/views/todo/todo_list_view.py
@@ -1,5 +1,10 @@
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+
+from todo_app.models import Todo
 
 @login_required
 def todo_list_view(request):
-    pass
+    todos = Todo.objects.filter(user=request.user).order_by('-created_at')
+    return render(request, 'todo/todo_list.html', {'todos': todos})
+
